eccf1cf5142a92e5072ca4d649684b83564c869f
Robert Haase <haesleinhuepf@users.noreply.github.com>
2021-12-26 14:59:49 +0100

    added test with assistant

--- src/napari_workflow_optimizer/_tests/test_dock_widget.py
+++ src/napari_workflow_optimizer/_tests/test_dock_widget.py
@@ -16,3 +16,55 @@ def test_something_with_viewer(widget_name, make_napari_viewer, napari_plugin_ma
         plugin_name=MY_PLUGIN_NAME, widget_name=widget_name
     )
     assert len(viewer.window._dock_widgets) == num_dw + 1
+
+
+def test_optimizer_with_assistant(make_napari_viewer):
+    viewer = make_napari_viewer()
+
+    # Add Assistant and configure workflow
+    from napari_pyclesperanto_assistant import Assistant
+    assistant = Assistant(viewer)
+
+    num_dw = len(viewer.window._dock_widgets)
+    viewer.window.add_dock_widget(assistant)
+    assert len(viewer.window._dock_widgets) == num_dw + 1
+
+    import numpy as np
+    image = np.asarray([
+        [0, 0, 0, 0, 0, 0, 0],
+        [1, 1, 1, 0, 0, 1, 1],
+        [1, 2, 1, 0, 0, 1, 2],
+        [1, 1, 1, 0, 0, 1, 1],
+        [0, 0, 0, 0, 0, 0, 0],
+        [0, 0, 1, 1, 1, 0, 0],
+        [0, 0, 1, 2, 1, 0, 0],
+    ])
+
+    viewer.add_image(image)
+
+    from napari_pyclesperanto_assistant._categories import CATEGORIES
+    assistant._activate(CATEGORIES.get("Label"))
+
+    # Add optimizer and optimize workflow
+    from napari_workflow_optimizer.gui._dock_widget import WorkflowOptimizer
+    optimizer_gui = WorkflowOptimizer(viewer)
+
+    num_dw = len(viewer.window._dock_widgets)
+    viewer.window.add_dock_widget(optimizer_gui)
+    assert len(viewer.window._dock_widgets) == num_dw + 1
+
+    optimizer_gui.labels_select.reset_choices()
+    optimizer_gui.reference_select.reset_choices()
+
+    optimizer_gui._plot_button("voronoi_otsu_labeling", "spot_sigma", 0)
+
+    optimizer_gui._enable_gui(True)
+    optimizer_gui.maxiter_select.value = 1
+
+    optimizer_gui._on_run_click()
+
+    optimizer_gui._on_undo_click()
+
+
+
+
